b519f3566e888ab5bb09b142f31af761690083bc
user <user@DESKTOP-BGPO6F5>
2023-03-22 17:16:07 +0900

    baekjoon-10807

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] array = new int[N];
+		int total=0;
+		for (int i = 0; i < N; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+
+		}
+		int V = Integer.parseInt(br.readLine());
+		for (int i = 0; i < array.length; i++) {
+			if(array[i]==V)
+			{
+				total++;
+			}
+		}
+		System.out.println(total);
+
+	}
+
+}
--- problem_solved.java
+++
@@ -1,9 +0,0 @@
-
-public class problem_solved {
-
-	public static void main(String[] args) {
-		System.out.println("Baekjoon");
-
-	}
-
-}
